f10a01634873ac8c556fb554a06eaf172de1aeb6
Issa <JeanMarieIssa@gmail.com>
2021-02-21 13:25:21 +0300

    Setting my files.

---
+++ src/Components/Cover.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Cover() {
+
+	const Container = styled.div`
+		color: red;
+	`;
+
+	return (
+		<Container>
+			Hellow rold
+		</Container>
+	)
+}
+
+export default Cover
