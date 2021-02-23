c6f4cf77d99016380d13eb021095fbb1f063115b
Issa <JeanMarieIssa@gmail.com>
2021-02-23 16:01:58 +0300

    singleMenu styled components fix.

--- src/Components/SingleMenu.jsx
+++ src/Components/SingleMenu.jsx
@@ -3,29 +3,12 @@ import styled from 'styled-components';
 
 function SingleMenu({type, number}) {
 
-	const Menu = styled.div`
-		background: #FFFFFF;
-		padding: 12px;
-		border-radius: 4px;
-		width: 130px;
-
-		h1{
-			color: #C1CF16;
-			font-size: 1.5em;
-		}
-
-		p{
-			font-weight: bold;
-			color: #0C0D0D;
-			font-size: 0.6em;
-		}
-	`;
 
 	return (
-		<Menu className="shadow-md">
+		<div className="singleMenu shadow-md">
 			<h1>{number}</h1>
 			<p>{type}</p>
-		</Menu>
+		</div>
 	)
 }
 
